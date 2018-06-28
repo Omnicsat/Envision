e4d699243dee07782dc341ddf149b456ce013ee5
unknown <retomnine@gmail.com>
2018-06-28 10:40:55 -0700

    Finish configure command, add elevate skeleton.

--- commands/elevate.js
+++ commands/elevate.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js')
-const fs = require("fs");
 
 module.exports = (client, message, args) => {
     let operativeUser = message.guild.member(message.mentions.users.first());
